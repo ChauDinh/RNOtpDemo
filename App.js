20bd9515a4f2744f916f18a2a6f4b380cce2169f
baochau.dinh <baochau.dinh@vexere.com>
2022-02-27 16:22:06 +0700

    suggest OTP Android

--- App.js
+++ App.js
@@ -6,107 +6,70 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  Keyboard,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
+import RNOtpVerify from 'react-native-otp-verify';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [otp, setOtp] = useState('');
+  useEffect(() => {
+    const getDeviceHashCode = async () => {
+      try {
+        const hash = await RNOtpVerify.getHash();
+        if (hash) {
+          console.log('device hash: ', hash)
+        }
+        return;
+      } catch (err) {
+        console.error(err)
+      }
+    }
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const getOtpFromMessage = async () => {
+      try {
+        const otp = await RNOtpVerify.getOtp();
+        if (otp) {
+          RNOtpVerify.addListener(handlerOTP);
+        }
+        return;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getDeviceHashCode()
+      .then(() => getOtpFromMessage())
+      .catch(err => console.error(err));
+  }, [otp, setOtp]);
+
+  const handlerOTP = message => {
+    if (!!message) {
+      const newOtp = /(\d{6})/g.exec(message)[1];
+      setOtp(newOtp);
+      RNOtpVerify.removeListener();
+      Keyboard.dismiss();
+    }
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+      <View 
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'black',
+        }}
+      >
+        <Text style={{color: 'white', marginBottom: 10}}>Let's learn to auto fill OTP 👨‍🎨</Text>
+        <Text style={{color: 'white'}}>{otp} 🤗</Text>
+        <TextInput />
+      </View>
   );
 };
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
